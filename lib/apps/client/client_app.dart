545c3b85502f14ab125872b891a6eef9789c9296
mahmoud <hhfjfjfj35@gmail.com>
2026-07-05 02:18:32 +0300

    feat: implement client authentication flow with onboarding screens, OTP verification, and locale-based flavor configuration

--- lib/apps/client/client_app.dart
+++ lib/apps/client/client_app.dart
@@ -13,6 +13,11 @@ import 'package:bmt_app/apps/client/features/trips/presentation/screens/my_trips
 import 'package:bmt_app/apps/client/features/trips/presentation/screens/trip_details_screen.dart';
 import 'package:bmt_app/core/localization/locale_cubit.dart';
 import 'package:bmt_app/l10n/app_localizations.dart';
+import 'package:bmt_app/apps/client/features/payments/domain/entities/payment_models.dart';
+import 'package:bmt_app/apps/client/features/payments/presentation/cubit/payment_cubit.dart';
+import 'package:bmt_app/apps/client/features/payments/presentation/screens/payment_checkout_screen.dart';
+import 'package:bmt_app/apps/client/features/packages/presentation/cubit/packages_cubit.dart';
+import 'package:bmt_app/apps/client/features/packages/presentation/screens/subscription_screen.dart';
 import 'package:bmt_app/apps/client/features/tracking/presentation/cubit/tracking_cubit.dart';
 import 'package:bmt_app/apps/client/features/tracking/presentation/screens/tracking_screen.dart';
 import 'package:bmt_app/apps/client/features/support/presentation/cubit/support_cubit.dart';
@@ -44,14 +49,19 @@ import 'package:bmt_app/apps/client/features/booking/presentation/routes/booking
 import 'package:bmt_app/apps/client/features/booking/domain/entities/booking_option.dart';
 import 'package:bmt_app/apps/client/features/booking/domain/entities/booking_search_query.dart';
 import 'package:bmt_app/apps/client/features/booking/presentation/cubit/booking_wizard_cubit.dart';
-
+import 'package:bmt_app/apps/client/features/booking/presentation/screens/available_trips_screen.dart';
 import 'package:bmt_app/apps/client/features/booking/presentation/screens/booking_approval_screen.dart';
 import 'package:bmt_app/apps/client/features/booking/presentation/screens/booking_wizard_screen.dart';
+import 'package:bmt_app/apps/client/features/booking/presentation/screens/daily_booking_flow_screen.dart';
 import 'package:bmt_app/apps/client/features/booking/presentation/screens/map_route_selection_screen.dart';
 import 'package:bmt_app/apps/client/features/booking/presentation/screens/popular_routes_screen.dart';
 import 'package:bmt_app/apps/client/features/booking/presentation/screens/route_overview_screen.dart';
 import 'package:bmt_app/apps/client/features/booking/presentation/screens/route_selection_screen.dart';
 import 'package:bmt_app/apps/client/features/booking/presentation/screens/search_trip_screen.dart';
+import 'package:bmt_app/apps/client/features/booking/presentation/screens/vehicle_details_screen.dart';
+import 'package:bmt_app/apps/client/features/booking/presentation/screens/vehicle_listing_screen.dart';
+import 'package:bmt_app/apps/client/features/seat_selection/presentation/cubit/seat_selection_cubit.dart';
+import 'package:bmt_app/apps/client/features/seat_selection/presentation/screens/seat_selection_screen.dart';
 import 'package:bmt_app/apps/client/features/seat_release/presentation/cubit/seat_release_cubit.dart';
 import 'package:bmt_app/apps/client/features/seat_release/presentation/screens/seat_release_screen.dart';
 import 'package:bmt_app/apps/client/core/theme/client_app_theme.dart';
@@ -61,8 +71,6 @@ import 'package:bmt_app/apps/client/features/onboarding/presentation/cubit/onboa
 import 'package:bmt_app/apps/client/features/onboarding/presentation/cubit/onboarding_state.dart';
 import 'package:bmt_app/apps/client/features/onboarding/presentation/screens/onboarding_screen.dart';
 import 'package:bmt_app/core/flavors/app_flavor.dart';
-import 'package:bmt_app/apps/client/features/packages/presentation/screens/subscription_screen.dart';
-import 'package:bmt_app/apps/client/features/packages/presentation/cubit/packages_cubit.dart';
 
 class ClientApp extends StatefulWidget {
   const ClientApp({super.key});
@@ -210,11 +218,22 @@ class _ClientAppState extends State<ClientApp> {
                 BookingRoutes.mapSelection: (_) =>
                     _buildBookingScope(const MapRouteSelectionScreen()),
                 BookingRoutes.availableTrips: (_) =>
-                    _buildBookingScope(const SearchTripScreen()),
+                    _buildBookingScope(const AvailableTripsScreen()),
                 BookingRoutes.vehicleListing: (_) =>
-                    _buildBookingScope(const SearchTripScreen()),
-                BookingRoutes.vehicleDetails: (_) =>
-                    _buildBookingScope(const SearchTripScreen()),
+                    _buildBookingScope(const VehicleListingScreen()),
+
+                BookingRoutes.vehicleDetails: (context) {
+                  final args = ModalRoute.of(context)?.settings.arguments;
+                  String? vehicleId;
+
+                  if (args is Map) {
+                    vehicleId = args['vehicleId']?.toString();
+                  }
+
+                  return _buildBookingScope(
+                    VehicleDetailsScreen(vehicleId: vehicleId),
+                  );
+                },
 
                 // New booking wizard flow
                 BookingRoutes.wizard: (context) {
@@ -247,18 +266,58 @@ class _ClientAppState extends State<ClientApp> {
                 },
 
                 '/daily-booking': (_) =>
-                    _buildBookingScope(const SearchTripScreen()),
+                    _buildBookingScope(const DailyBookingFlowScreen()),
                 '/seat-selection': (_) =>
-                    _buildBookingScope(const SearchTripScreen()),
+                    _buildSeatSelectionScope(const SeatSelectionScreen()),
                 '/seat-release': (_) =>
                     _buildSeatReleaseScope(const SeatReleaseScreen()),
-                '/payment-checkout': (_) =>
-                    _buildBookingScope(const SearchTripScreen()),
+
+                '/payment-checkout': (context) {
+                  final args = ModalRoute.of(context)?.settings.arguments;
+                  final checkoutData = args is Map
+                      ? PaymentCheckoutData(
+                          tripId: args['tripId']?.toString() ?? '',
+                          pickupPoint: args['pickupPoint']?.toString() ?? '',
+                          destination: args['destination']?.toString() ?? '',
+                          vehicleNumber:
+                              args['vehicleNumber']?.toString() ?? '',
+                          tripDate: args['tripDate']?.toString() ?? '',
+                          departureTime:
+                              args['departureTime']?.toString() ?? '',
+                          arrivalTime: args['arrivalTime']?.toString() ?? '',
+                          selectedSeatId:
+                              args['selectedSeatId']?.toString() ?? '',
+                          selectedSeat: args['selectedSeat']?.toString() ?? '',
+                          driverName: args['driverName']?.toString() ?? '',
+                          baseFare: _parseMoney(args['baseFare']),
+                          serviceFee: _parseMoney(args['serviceFee']),
+                          tax: _parseMoney(args['tax']),
+                        )
+                      : PaymentCheckoutData(
+                          tripId: '',
+                          pickupPoint: '',
+                          destination: '',
+                          vehicleNumber: '',
+                          tripDate: '',
+                          departureTime: '',
+                          arrivalTime: '',
+                          selectedSeatId: '',
+                          selectedSeat: '',
+                          driverName: '',
+                        );
+
+                  return _buildPaymentScope(
+                    PaymentCheckoutScreen(checkoutData: checkoutData),
+                  );
+                },
+
                 '/subscription': (context) {
-                  final args = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>? ?? {};
-                  return _buildPackagesScope(SubscriptionScreen(
-                    bookingData: args,
-                  ));
+                  final args = ModalRoute.of(context)?.settings.arguments;
+                  final hasActiveSub =
+                      args is Map && args['hasActiveSubscription'] == true;
+                  return _buildPackagesScope(
+                    SubscriptionScreen(hasActiveSubscription: hasActiveSub),
+                  );
                 },
 
                 // Trips
@@ -339,6 +398,11 @@ class _ClientAppState extends State<ClientApp> {
     );
   }
 
+  int _parseMoney(Object? value) {
+    if (value is num) return value.round();
+    return num.tryParse(value?.toString() ?? '')?.round() ?? 0;
+  }
+
   Widget _buildClientShell() {
     return ClientShellScreen(
       routesBuilder: (context) => _buildRoutesHubScope(
@@ -395,7 +459,12 @@ class _ClientAppState extends State<ClientApp> {
     );
   }
 
-
+  Widget _buildSeatSelectionScope(Widget child) {
+    return BlocProvider<SeatSelectionCubit>(
+      create: (_) => clientGetIt<SeatSelectionCubit>(),
+      child: child,
+    );
+  }
 
   Widget _buildSeatReleaseScope(Widget child) {
     return BlocProvider<SeatReleaseCubit>(
@@ -404,7 +473,12 @@ class _ClientAppState extends State<ClientApp> {
     );
   }
 
-
+  Widget _buildPaymentScope(Widget child) {
+    return BlocProvider<PaymentCubit>(
+      create: (_) => clientGetIt<PaymentCubit>(),
+      child: child,
+    );
+  }
 
   Widget _buildPackagesScope(Widget child) {
     return BlocProvider<PackagesCubit>(
--- lib/apps/client/core/di/client_di.dart
+++ lib/apps/client/core/di/client_di.dart
@@ -137,7 +137,6 @@ import '../../features/trips/data/repositories/trips_repository_impl.dart';
 import '../../features/trips/domain/repositories/trips_repository.dart';
 import '../../features/trips/domain/usecases/get_trip_details_usecase.dart';
 import '../../features/trips/domain/usecases/get_trips_usecase.dart';
-import '../../features/trips/domain/usecases/watch_trips_usecase.dart';
 import '../../features/trips/presentation/cubit/trips_cubit.dart';
 import '../../features/tracking/data/datasources/supabase_tracking_datasource.dart';
 import '../../features/tracking/data/repositories/tracking_repository_impl.dart';
@@ -145,7 +144,6 @@ import '../../features/tracking/domain/repositories/tracking_repository.dart';
 import '../../features/tracking/domain/usecases/get_tracking_title_usecase.dart';
 import '../../features/tracking/domain/usecases/get_tracking_trip_usecase.dart';
 import '../../features/tracking/domain/usecases/watch_vehicle_position_usecase.dart';
-import '../../features/tracking/domain/usecases/watch_tracking_trip_usecase.dart';
 import '../../features/tracking/presentation/cubit/tracking_cubit.dart';
 import '../../../../core/network/network_di.dart';
 
@@ -318,18 +316,11 @@ void _registerTripsDependencies() {
     );
   }
 
-  if (!clientGetIt.isRegistered<WatchTripsUseCase>()) {
-    clientGetIt.registerLazySingleton<WatchTripsUseCase>(
-      () => WatchTripsUseCase(clientGetIt<TripsRepository>()),
-    );
-  }
-
   if (!clientGetIt.isRegistered<TripsCubit>()) {
     clientGetIt.registerFactory<TripsCubit>(
       () => TripsCubit(
         getTrips: clientGetIt<GetTripsUseCase>(),
         getTripDetails: clientGetIt<GetTripDetailsUseCase>(),
-        watchTrips: clientGetIt<WatchTripsUseCase>(),
       ),
     );
   }
@@ -676,19 +667,12 @@ void _registerTrackingDependencies() {
     );
   }
 
-  if (!clientGetIt.isRegistered<WatchTrackingTripUseCase>()) {
-    clientGetIt.registerLazySingleton<WatchTrackingTripUseCase>(
-      () => WatchTrackingTripUseCase(clientGetIt<TrackingRepository>()),
-    );
-  }
-
   if (!clientGetIt.isRegistered<TrackingCubit>()) {
     clientGetIt.registerFactory<TrackingCubit>(
       () => TrackingCubit(
         getTrackingTrip: clientGetIt<GetTrackingTripUseCase>(),
         getTrackingTitle: clientGetIt<GetTrackingTitleUseCase>(),
         watchVehiclePosition: clientGetIt<WatchVehiclePositionUseCase>(),
-        watchTrackingTrip: clientGetIt<WatchTrackingTripUseCase>(),
       ),
     );
   }
