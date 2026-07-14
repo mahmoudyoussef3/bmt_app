4f56ae1b32598b963817785b8c43f67b74f0eaf3
mahmoud <hhfjfjfj35@gmail.com>
2026-07-14 23:59:49 +0300

    fix bugs

--- lib/apps/client/features/support/domain/entities/support_workspace.dart
+++
@@ -1,8 +0,0 @@
-import 'support_ticket.dart';
-
-class SupportWorkspace {
-  const SupportWorkspace({required this.categories, required this.tickets});
-
-  final List<String> categories;
-  final List<SupportTicket> tickets;
-}
--- lib/apps/dashboard/features/live_trips/domain/usecases/watch_live_trips_usecase.dart
+++
@@ -1,9 +0,0 @@
-import '../repositories/live_trips_repository.dart';
-
-class WatchLiveTripsUseCase {
-  const WatchLiveTripsUseCase(this._repository);
-
-  final LiveTripsRepository _repository;
-
-  Stream<void> call() => _repository.watchTripStatusChanges();
-}
